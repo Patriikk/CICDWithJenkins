f3a7ab9a39981a1e25c69dee849af0232b7d0da2
PratikFagadiya <pratikf.codezee@gmail.com>
2023-09-01 12:30:19 +0530

    Update Design

--- app/src/main/java/com/patrik/cicdwithjenkins/MainActivity.kt
+++ app/src/main/java/com/patrik/cicdwithjenkins/MainActivity.kt
@@ -3,13 +3,20 @@ package com.patrik.cicdwithjenkins
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.patrik.cicdwithjenkins.ui.theme.CICDWithJenkinsTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,9 +37,22 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!", modifier = modifier
-    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = "Hello $name!", modifier = modifier, style = TextStyle(
+                fontSize = 22.sp
+            )
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
